77e89f8d8a60817636aeba6facc74fbdf9dd04df
ajazgar <asia.jazgar@gmail.com>
2019-01-07 22:23:03 +0100

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { SearchComponent } from './search/search.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { SearchComponent } from './shared/search/search.component';
 
 @NgModule({
   declarations: [
