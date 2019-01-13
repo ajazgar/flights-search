8bfaf3aace02c7cfac4e817498a4b6a0da2dd13b
ajazgar <asia.jazgar@gmail.com>
2019-01-13 22:38:26 +0100

    styles+routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HotelsComponent} from './pages/hotels/hotels.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {CarsComponent} from './pages/cars/cars.component';
+import {DiscoverComponent} from './pages/discover/discover.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'hotele', component: HotelsComponent},
+  {path: 'samochody', component: CarsComponent},
+  {path: 'discover', component: DiscoverComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { SearchComponent } from './shared/search/search.component';
+import { HotelsComponent } from './pages/hotels/hotels.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CarsComponent } from './pages/cars/cars.component';
+import { DiscoverComponent } from './pages/discover/discover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SearchComponent
+    SearchComponent,
+    HotelsComponent,
+    DashboardComponent,
+    CarsComponent,
+    DiscoverComponent
   ],
   imports: [
     BrowserModule,
